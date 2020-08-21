bea9b5ef2ea89b28c66f809d844626a43f01125f
dreamume <dreamume@gmail.com>
2020-08-21 18:06:18 +0800

    feat(euclid): add modular linear equation solver method

--- chapter31/euclid.cc
+++ chapter31/euclid.cc
@@ -19,12 +19,26 @@ void CEuclid::extended_euclid(int a, int b) {
   y_ = y;
 }
 
+void CEuclid::modular_linear_equatiion_solver(int a, int b, int n) {
+  extended_euclid(a, n);
+  if (b % d_ == 0) {
+    int res = x_ * (b / d_) % n;
+    while (res < 0) res += n;
+    for (int i = 0; i < d_; ++i)
+      printf("%d ", ((res + i * (n / d_)) % n));
+    printf("\n");
+  } else {
+    printf("no solutions\n");
+  }
+}
+
 #if Debug
 int main(int argc, char *argv[]) {
   CEuclid euclid;
-  euclid.extended_euclid(899, 493);
-  printf("d is %d, x is %d, y is %d, ax + by = %d\n", 
-         euclid.d(), euclid.x(), euclid.y(), 899 * euclid.x() + 493 * euclid.y());
+  //euclid.extended_euclid(899, 493);
+  // printf("d is %d, x is %d, y is %d, ax + by = %d\n", 
+  //        euclid.d(), euclid.x(), euclid.y(), 899 * euclid.x() + 493 * euclid.y());
+  euclid.modular_linear_equatiion_solver(14, 30, 100);
   
   return 0;
 }
